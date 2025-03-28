5c6a58e77054d9dc7c55abbc4cf31befa26c4b30
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-28 10:34:38 -0300

    adicionando a configuracao CORS  e mais uma variavel nome na classe reservation

--- src/main/java/br/com/fiap/reserva_Sovrano/model/Reservation.java
+++ src/main/java/br/com/fiap/reserva_Sovrano/model/Reservation.java
@@ -15,6 +15,7 @@ import lombok.Data;
 public class Reservation {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idReserva;
+    private String nome;
     private LocalDateTime reservationDate;
     private int quantPessoas;
     private StatusReserva status=StatusReserva.CONFIRMADA;
